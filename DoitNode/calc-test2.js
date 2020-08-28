c7e9a510d33c6d2949de81da69b717995c3ce2f1
Lee <dlehdud1653@kakao.com>
2020-08-28 12:12:23 +0900

    delete doit

--- DoitNode/calc-test2.js
+++
@@ -1,3 +0,0 @@
-var calc3 = require('./calc3')
-
-console.log('모듈로 분리한 후 - calc3.add : ' + calc3.add(10,10))
--- DoitNode/calc3.js
+++
@@ -1,8 +0,0 @@
-var calc = {}
-
-calc.add = function(a, b) {
-    return a + b
-}
-
-// 객체를 직접 할당
-module.exports = calc;
